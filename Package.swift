6df1f2bf8051b551e5e51ddf2901394327efcf4b
STREGA <STREGA@users.noreply.github.com>
2023-07-27 19:16:39 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -46,7 +46,7 @@ let package: Package = Package(
         .executable(name: "3D_FirstPerson", targets: ["3D_FirstPerson"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/STREGAsGate/GateEngine.git", branch: "main"),
+        .package(url: "https://github.com/STREGAsGate/GateEngine.git", branch: "add-gravity"),
     ],
     targets: [
         .executableTarget(name: "_01_UserInput",
